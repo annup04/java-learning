4c73a6ff8a2ef43a7405bcc271b0973280fffefe
Annu Priya <annuchy8265@gmail.com>
2026-03-14 23:45:33 +0530

    DAY:-5

---
+++ switchstam.java
@@ -0,0 +1,15 @@
+public class switchstam {
+    public static void main(String[] args) {
+       int number =2;
+       switch (number) {
+        case 1: System.out.println("samosa");
+         break;
+        case 2: System.out.println("Burger");
+        break;
+        case 3: System.out.println("cold coffe");
+       break;
+        default : System.out.println("veg food");
+       } 
+    }
+}
+ 
