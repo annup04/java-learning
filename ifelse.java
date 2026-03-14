f988991f4ffddde90bcf7f63f12cb3573302f144
Annu Priya <annuchy8265@gmail.com>
2026-03-14 22:15:28 +0530

    Day-5

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+public class ifelse {
+    public static void main(String[] args) {
+       // int age=22;
+       int age=16;
+        if (age >=18) {
+            System.out.println("vote");
+        }
+        if(age>13 && age<18){
+            System.out.println("teenager");
+        }
+        else{
+            System.out.println("not adult");
+        }
+    }   
+        }
+        //OUTPUT:-VOTE
+//2 OUT:-Teenager
+
