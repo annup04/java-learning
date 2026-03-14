1a68b4f45382e288839232f9b07f82f8ebc4a9bb
Annu Priya <annuchy8265@gmail.com>
2026-03-14 23:07:48 +0530

    DAY:-5

---
+++ largof3.java
@@ -0,0 +1,19 @@
+public class largof3 {
+    public static void main(String[] args) {
+        int a=1;
+        int b=3;
+        int c=6;
+        if ((a>=b) && (a>=c)){
+         System.out.println("largest is a");
+        }
+else if(b>=c){
+    System.out.println("largest is b");
+}
+else {
+    System.out.println("largest is c");
+}
+}
+        }
+ /* OUTPUT:- largest is c */   
+    
+
