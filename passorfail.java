717665dc78f3f6f7acb5147a73c69485da567775
Annu Priya <annuchy8265@gmail.com>
2026-03-14 23:20:24 +0530

    DAY:-5

---
+++ passorfail.java
@@ -0,0 +1,10 @@
+public class passorfail {
+    public static void main(String[] args) {
+        /* ternary operator */
+        int marks = 67;
+      String reportCard= marks >= 33 ? "pass" : "fail";
+      System.out.println(reportCard); 
+    }
+    
+}
+//OUTPUT :- pass
