b0b36a28ae89ae4b126b1a6402b6f77d523eb5e4
Annu Priya <annuchy8265@gmail.com>
2026-03-14 22:23:35 +0530

    DAY:-5

---
+++ larof2.java
@@ -0,0 +1,14 @@
+public class larof2 {
+    public static void main(String[] args) {
+      //if-else statment
+        int A=1;
+        int B=5;
+        if(A>=B){
+            System.out.println("A is largest of 2");
+      }else{
+        System.out.println("B is largest of 2");
+      }
+    }
+    
+}
+/*OUTPUT:- B is largest of 2 */
