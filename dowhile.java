63747e55b7788b192ab070f898de348c7198c41c
Annu Priya <annuchy8265@gmail.com>
2026-03-16 06:51:25 +0530

    DAY:-7

---
+++ dowhile.java
@@ -0,0 +1,10 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int counter=1;
+        do{
+            System.out.println("GOOD MORNING");
+counter++;       
+ } while(counter<=10);
+    }
+    
+}
