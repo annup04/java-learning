1e817ffb225b4a17db861da884ebd8c1635a198f
Annu Priya <annuchy8265@gmail.com>
2026-03-16 07:34:26 +0530

    DAY:-7

---
+++ primeornot.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class primeornot {
+   //prime no :-2,3,5,7,11 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+if (n==2) {
+    System.out.println("n is prime");
+}else{
+    System.out.println("Not prime");
+}
+        boolean isPrime =true;
+        for(int i=2;i<=n-1;i++){
+            if(n%i ==0){//n is a multiple of i(i not equal to 1 or n)
+                isPrime=false;
+            }
+        }
+        if(isPrime==true){
+            System.out.println("n is prime");
+      } else{
+        System.out.println("no is not prime");
+      }
+    }
+}
