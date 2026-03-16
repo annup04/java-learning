1137981ba2f73fadd4550906ef238d48714ce6ff
Annu Priya <annuchy8265@gmail.com>
2026-03-16 18:06:07 +0530

    DAY:-7

---
+++ Invertedstar.java
@@ -0,0 +1,12 @@
+public class Invertedstar {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= 4; i++) {
+            for (int s = 1; s <= 4 -i + 1; s++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
