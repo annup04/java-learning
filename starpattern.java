8fefb885828346d7680a0f9fbf9284282de36891
Annu Priya <annuchy8265@gmail.com>
2026-03-16 17:50:58 +0530

    Day:-7

---
+++ starpattern.java
@@ -0,0 +1,12 @@
+public class starpattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            //one line
+            for (int star = 1; star <= i; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
