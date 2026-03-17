26b12491936728888eecd69312c28551fdeb3ca7
Annu Priya <annuchy8265@gmail.com>
2026-03-17 06:01:32 +0530

    DAY:-8

--- funcparameters.java
+++ funcparameters.java
@@ -1,22 +1,17 @@
 import java.util.Scanner;
 
 public class funcparameters {
-    public static void printHelloWorld() {
-        System.out.println("HEllo world");
-        System.out.println("HEllo world");
-        System.out.println("HEllo world");
-        return;
+
+
+    public static void calculatesum(int num1, int num2) {
+        int sum = num1 + num2;
+        System.out.println("sum is :" + sum);
     }
 
-    public static void calculateSum(){
- Scanner sc= new Scanner (System.in);
-   int a =sc.nextInt();
-   int b=sc.nextInt();
-   int sum =a+b;
-System.out.println("sum is"+sum);
-    }
-
-    public static void main(String[] args) {
-        calculateSum();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        calculatesum(a, b);
     }
 }
