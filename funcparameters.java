62269e526dc782c35aa44cd960634734e7520c0c
Annu Priya <annuchy8265@gmail.com>
2026-03-17 06:08:36 +0530

    Day:-8

--- funcparameters.java
+++ funcparameters.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class funcparameters {
 
 
-    public static void calculatesum(int num1, int num2) {
+    public static void calculatesum(int num1, int num2)//parameters or formal parameters
+    {
         int sum = num1 + num2;
         System.out.println("sum is :" + sum);
     }
@@ -12,6 +13,6 @@ public class funcparameters {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        calculatesum(a, b);
+        calculatesum(a, b);//arguments or actual parameters
     }
 }
