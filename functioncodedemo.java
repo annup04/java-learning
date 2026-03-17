7742fcd3e824e69ffd6d7e7247587ad5ecec4093
Annu Priya <annuchy8265@gmail.com>
2026-03-17 05:39:34 +0530

    DAY:-8

---
+++ functioncodedemo.java
@@ -0,0 +1,11 @@
+public class functioncodedemo {
+    public static void printHelloWorld(){
+        System.out.println("HEllo world");
+        System.out.println("HEllo world");
+       System.out.println("HEllo world");
+       return;
+    }
+    public static void main(String[] args) {
+        printHelloWorld(); //function call
+    }
+}
