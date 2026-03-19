ac8798a392b41bfbc76cf28749ab91b2293776d3
Annu Priya <annuchy8265@gmail.com>
2026-03-19 23:23:08 +0530

    DAY:-10

--- productofab.java
+++ productofab.java
@@ -1,14 +1,3 @@
-public class  productofab {
-
-    public static int multiply(int a, int b) {
-        int product = a * b;
-        return product;
-    }
-
-    public static void main(String[] args) {
-        int a = 3;
-        int b = 5;
-        int prod = multiply(a, b);
-        System.out.println("a*b =" + prod);
-    }
+public class productofab {
+    
 }
