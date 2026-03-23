ced3ddcde0853aae48f5fa42df898967ada59241
Annu Priya <annuchy8265@gmail.com>
2026-03-23 23:03:08 +0530

    DAY:-14

---
+++ pallprimeinrange.java
@@ -0,0 +1,26 @@
+public class pallprimeinrange {
+    public static void primesInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {// true
+
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+    
+    public static boolean isPrime(int num){
+        if(num<2) return false;
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        primesInRange(20);
+    }
+
+}
