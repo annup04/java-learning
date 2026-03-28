6192bb8dacd361694be64bfa051341361737d224
Annu Priya <annuchy8265@gmail.com>
2026-03-28 18:16:24 +0530

    DAY:-19

---
+++ invertedrotatedhalfpyramid.java
@@ -0,0 +1,19 @@
+public class invertedrotatedhalfpyramid {
+    public static void inverted_rotated_half_pyramid(int n ){
+        //outer
+        for (int i=1;i<=n;i++){
+            //spaces
+            for (int j =1; j<+n-i;j++){
+                System.out.println(" ");
+            }
+            //star
+            for(int j=1;j<=i;j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        inverted_rotated_half_pyramid(4);
+    }
+}
